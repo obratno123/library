c3e27fe54de6a58d4a361a7e9ef7a3c7b4dce6d7
bl1nchik2287 <151465257+bl1nchik2287@users.noreply.github.com>
2026-04-14 19:44:00 +0300

    deleted unnecessary tests

--- review_rating/tests.py
+++ review_rating/tests.py
@@ -9,7 +9,7 @@ from review_rating.models import Review
 class reviewmodelTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
-            username="ivan",
+            username="`",
             password="testpass123"
         )
 
@@ -19,16 +19,6 @@ class reviewmodelTests(TestCase):
             isbn="1234567890999",
             price=Decimal("500.00"),
         )
-
-    def test_str__review_id_and_username(self):
-        review = Review.objects.create(
-            user=self.user,
-            book=self.book,
-            rating=5,
-            text="Очень хорошая книга",
-        )
-        
-        self.assertEqual(str(review), f"Отзыв #{review.id} - ivan")
         
     def test_correct_user_and_book(self):
         review = Review.objects.create(
@@ -41,12 +31,3 @@ class reviewmodelTests(TestCase):
         self.assertEqual(review.user, self.user)
         self.assertEqual(review.book, self.book)
         
-    def test_review_is_published_true(self): ### можно удалить
-        review = Review.objects.create(
-            user=self.user,
-            book=self.book,
-            rating=4,
-            text="Нормально",
-        )
-
-        self.assertTrue(review.is_published)
