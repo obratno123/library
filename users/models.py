896a6827682323b55bd14f851fff35a468eb5a65
obratno123 <hailbb.hh@gmail.com>
2026-04-14 19:15:07 +0300

    добавил восстановление пароля по коду на почту

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 
 class Role(models.Model):
@@ -58,3 +62,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.full_name
+    
+class PasswordResetCode(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    code_hash = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_used = models.BooleanField(default=False)
+
+    def is_expired(self):
+        return timezone.now() > self.created_at + timedelta(minutes=10)
+
+    def __str__(self):
+        return f"Reset code for {self.user}"
